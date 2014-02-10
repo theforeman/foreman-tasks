cf3f5bd11b2360307ad55e1e363fe3a9f22a58e3
Petr Chalupa <git@pitr.ch>
2014-02-10 15:07:21 +0100

    Add ForemanTasks::Triggers module to include trigger methods where needed

--- lib/foreman_tasks.rb
+++ lib/foreman_tasks.rb
@@ -1,8 +1,10 @@
 require 'foreman_tasks/version'
 require 'foreman_tasks/engine'
 require 'foreman_tasks/dynflow'
+require 'foreman_tasks/triggers'
 
 module ForemanTasks
+  extend Algebrick::TypeCheck
 
   def self.dynflow
     @dynflow ||= ForemanTasks::Dynflow.new
@@ -12,9 +14,21 @@ module ForemanTasks
     dynflow.world.trigger action, *args, &block
   end
 
-  def self.async_task(action, *args, &block)
+  def self.trigger_task(async, action, *args, &block)
+    Match! async, true, false
+
     run = trigger(action, *args, &block)
-    ForemanTasks::Task::DynflowTask.find_by_external_id(run.id)
+    run.finished.wait if async == false
+    ForemanTasks::Task::DynflowTask.find_by_external_id!(run.id)
+  end
+
+  def self.async_task(action, *args, &block)
+    trigger_task true, action, *args, &block
+  end
+
+  def self.sync_task(action, *args, &block)
+    # TODO raise aggregation error when there are failed run-steps
+    trigger_task false, action, *args, &block
   end
 
 end
