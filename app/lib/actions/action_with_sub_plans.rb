131f05b6fcc027d071cc4ad71610827f7dff9161
Ivan Nečas <inecas@redhat.com>
2015-02-23 16:18:48 +0100

    Fixes #9508 - extract the triggering and waiting for sub plans from bulk actions
    Bulk actions is just one of the use-cases for using sub-plans. This
    patch extract the common code to be used for triggering external tasks
    in general.
    
    The usage is as follows: one needs to inherit it's action
    from the ``Actions::ActionWithSubPlans`` and override the
    ``create_sub_plans`` method. The ``create_sub_plans`` will be executed
    in run phase and the rest of the action will make sure that the
    execution of the action will wait till the sub plan is done. It also
    tracks the relation between parent and child tasks, as it already did
    in bulk actions.
    
    An example code could look like this (taken from real Katello actions):
    
        class SyncAsSubPlan < Actions::ActionWithSubPlans
          input_format do
            param :id, Integer
          end
    
          def plan(repository)
            plan_self(:id => repository.id)
          end
    
          def create_sub_plans
            trigger(Repository::Sync, ::Katello::Repository.find(input[:id]))
          end
        end

---
+++ app/lib/actions/action_with_sub_plans.rb
@@ -0,0 +1,30 @@
+module Actions
+
+  class Actions::ActionWithSubPlans < Actions::EntryAction
+
+    middleware.use Actions::Middleware::KeepCurrentUser
+
+    include Dynflow::Action::WithSubPlans
+
+    def plan(*args)
+      raise NotImplementedError
+    end
+
+    def humanized_output
+      return unless counts_set?
+      _("%{total} tasks, %{success} success, %{failed} fail") %
+          { total:   output[:total_count],
+            success: output[:success_count],
+            failed:  output[:failed_count] }
+    end
+
+    def run_progress
+      if counts_set? && output[:total_count] > 0
+        (output[:success_count] + output[:failed_count]).to_f / output[:total_count]
+      else
+        0.1
+      end
+    end
+
+  end
+end
