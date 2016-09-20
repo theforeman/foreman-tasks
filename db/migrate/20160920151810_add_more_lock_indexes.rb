1c27abf3a04e462f71311cbfca2a281aec609542
Ivan Nečas <inecas@redhat.com>
2016-09-20 15:25:37 +0200

    Fixes #15283 - add more indexes the locks
    We are searching on various lock attributes to find out if we don't
    have any active task locking the resource. If the amount of locks
    it too high, it can lead to performance degradation.

---
+++ db/migrate/20160920151810_add_more_lock_indexes.rb
@@ -0,0 +1,9 @@
+class AddMoreLockIndexes < ActiveRecord::Migration
+  def change
+    add_index(:foreman_tasks_tasks, [:id, :state],
+              :name => 'index_foreman_tasks_id_state')
+    add_index(:foreman_tasks_locks, [:name, :resource_type, :resource_id],
+              :name => 'index_foreman_tasks_locks_name_resource_type_resource_id')
+  end
+
+end
