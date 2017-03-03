c3d004cd5bdc709baeba976a3e85db784e849767
Lukas Zapletal <lzap+git@redhat.com>
2017-03-03 14:25:26 +0100

    Fixes #18231 - task export moved to dynflow

--- extra/dynflow-debug.sh
+++ extra/dynflow-debug.sh
@@ -3,5 +3,17 @@
 # This file provides additional debug information for foreman-debug tool and is
 # symlinked as /usr/share/foreman/script/foreman-debug.d/60-dynflow_debug
 
+export_csv() {
+  echo "COPY ($1) TO STDOUT WITH CSV;" | su - postgres -c '/usr/bin/psql -d foreman' > $2
+}
+
 add_files /var/log/foreman/dynflow_executor*.log*
 add_files /var/log/foreman/dynflow_executor*.output*
+
+# Foreman Tasks fast export (for HTML version use foreman-rake foreman_tasks:export_tasks)
+export_csv "select dynflow_execution_plans.* from foreman_tasks_tasks join dynflow_execution_plans on (foreman_tasks_tasks.external_id = dynflow_execution_plans.uuid) where foreman_tasks_tasks.started_at > 'now'::timestamp - '${DYNFLOW_EXPORT_MONTHS:-6} months'::interval" "$DIR/dynflow_execution_plans.csv"
+export_csv "select dynflow_actions.* from foreman_tasks_tasks join dynflow_actions on (foreman_tasks_tasks.external_id = dynflow_actions.execution_plan_uuid) where foreman_tasks_tasks.started_at > 'now'::timestamp - '${DYNFLOW_EXPORT_MONTHS:-6} months'::interval" "$DIR/dynflow_actions.csv"
+export_csv "select dynflow_steps.* from foreman_tasks_tasks join dynflow_steps on (foreman_tasks_tasks.external_id = dynflow_steps.execution_plan_uuid) where foreman_tasks_tasks.started_at > 'now'::timestamp - '${DYNFLOW_EXPORT_MONTHS:-6} months'::interval" "$DIR/dynflow_steps.csv"
+export_csv "select * from dynflow_schema_info" "$DIR/dynflow_schema_info.csv"
+export_csv "select * from foreman_tasks_tasks" "$DIR/foreman_tasks_tasks.csv"
+
