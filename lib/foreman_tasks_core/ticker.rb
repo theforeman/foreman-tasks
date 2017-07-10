af1eb5e81a75c6d5fb479ca587350aae6ec0f2d3
adamruzicka <adamruzicka@users.noreply.github.com>
2017-07-10 16:39:13 +0200

    Fixes #20182 - Handle clock ticking more efficiently (#262)
    * Fixes #20182 - Handle clock ticking more efficiently

---
+++ lib/foreman_tasks_core/ticker.rb
@@ -0,0 +1,45 @@
+module ForemanTasksCore
+  class Ticker < ::Dynflow::Actor
+    attr_reader :clock
+
+    def initialize(clock, logger)
+      @clock = clock
+      @logger = logger
+      @events = []
+      @refresh_interval = 1
+      plan_next_tick
+    end
+
+    def tick
+      @logger.debug("Ticker ticking for #{@events.size} events")
+      @events.each do |(target, args)|
+        pass_event(target, args)
+      end
+      @events = []
+    ensure
+      @planned = false
+      plan_next_tick
+    end
+
+    def add_event(target, args)
+      @events << [target, args]
+      plan_next_tick
+    end
+
+    private
+
+    def pass_event(target, args)
+      target.tell(args)
+    rescue => e
+      @logger.error("Failed passing event to #{target} with #{args}")
+      @logger.error(e)
+    end
+
+    def plan_next_tick
+      if !@planned && !@events.empty?
+        @clock.ping(reference, Time.now.getlocal + @refresh_interval, :tick)
+        @planned = true
+      end
+    end
+  end
+end
