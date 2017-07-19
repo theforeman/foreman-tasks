b266d39206a44c619fb859222abdc0b82e0a045e
Tomer Brisker <tbrisker@gmail.com>
2017-07-19 11:46:22 +0300

    Fixes #20341 - status widget shows better info
    Added column showing last task start time and removed successful tasks
    from the list since they are not interesting

--- app/models/foreman_tasks/task/summarizer.rb
+++ app/models/foreman_tasks/task/summarizer.rb
@@ -1,7 +1,9 @@
 module ForemanTasks
   class Task::Summarizer
     def summarize_by_status(since = nil)
-      result = ::ForemanTasks::Task.select('count(state) AS count, state, result').group(:state, :result).order(:state)
+      result = ::ForemanTasks::Task.where("result <> 'success'")
+                                   .select('count(state) AS count, state, result, max(started_at) AS started_at')
+                                   .group(:state, :result).order(:state)
       result = result.where('started_at > ?', since) if since
       result
     end
