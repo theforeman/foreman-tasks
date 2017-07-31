d0d7cf48f1d9b1c9cf99e31e288773ead66d4692
Adam Ruzicka <aruzicka@redhat.com>
2017-07-31 13:40:24 +0200

    Fixes #20443 - Add per_page to recurring_logics controller

--- app/controllers/foreman_tasks/recurring_logics_controller.rb
+++ app/controllers/foreman_tasks/recurring_logics_controller.rb
@@ -28,7 +28,8 @@ module ForemanTasks
     end
 
     def filter(scope)
-      scope.search_for(params[:search]).paginate(:page => params[:page])
+      scope.search_for(params[:search])
+           .paginate(:page => params[:page], :per_page => params[:per_page])
     end
   end
 end
