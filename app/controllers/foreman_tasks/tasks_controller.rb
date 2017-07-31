ce51bc05fe1bbf4e250746f2a5bbacb2de7108bb
Adam Ruzicka <aruzicka@redhat.com>
2017-07-31 11:03:37 +0200

    Fixes #20443 - Add per_page filtering to tasks

--- app/controllers/foreman_tasks/tasks_controller.rb
+++ app/controllers/foreman_tasks/tasks_controller.rb
@@ -107,7 +107,7 @@ module ForemanTasks
 
     def filter(scope)
       scope.search_for(params[:search], :order => params[:order])
-           .paginate(:page => params[:page]).distinct
+           .paginate(:page => params[:page], :per_page => params[:per_page]).distinct
     end
   end
 end
