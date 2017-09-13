59eaba2bdbbce5ae4e3a1f6341bed2e9e138fca8
Martin Bačovský <martin.bacovsky@gmail.com>
2017-09-13 22:00:01 +0200

    Fixes #20914 - Connect to configured Foreman DB during export
    foreman-debug extension was updated to use DB from Foreman configuration
    to export foreman-tasks DB data. Before the patch connection to local
    postgresql server and database 'foreman' was hard-coded.

--- extra/dynflow-debug.sh
+++ extra/dynflow-debug.sh
@@ -3,8 +3,11 @@
 # This file provides additional debug information for foreman-debug tool and is
 # symlinked as /usr/share/foreman/script/foreman-debug.d/60-dynflow_debug
 
+GET_DB_CONFIG_SCRIPT='db = YAML.load_file("/etc/foreman/database.yml")["production"];puts %(PGPASSWORD="#{db["password"]}" psql -U #{db["username"]} -h "#{db["host"]||"localhost"}" -p #{db["port"]||"5432"} -d #{db["database"]})'
+FOREMAN_PSQL_COMMAND=`ruby -ryaml -e "$GET_DB_CONFIG_SCRIPT"`
+
 export_csv() {
-  echo "COPY ($1) TO STDOUT WITH CSV;" | su - postgres -c '/usr/bin/psql -d foreman' > $2
+  echo "COPY ($1) TO STDOUT WITH CSV;" | eval $FOREMAN_PSQL_COMMAND > $2
 }
 
 add_files /var/log/foreman/dynflow_executor*.log*
