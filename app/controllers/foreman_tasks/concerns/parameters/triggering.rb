1bd22c1b079f9b49010d86117195d90768d2caed
Tomer Brisker <tbrisker@gmail.com>
2018-01-28 15:03:48 +0200

    Fixes #22438 - Remove KeepParams
    KeepParams was created as a workaround for an issue in strong
    params prior to Rails 5.1. It is no longer required.

--- app/controllers/foreman_tasks/concerns/parameters/triggering.rb
+++ app/controllers/foreman_tasks/concerns/parameters/triggering.rb
@@ -3,22 +3,28 @@ module ForemanTasks
     module Parameters
       module Triggering
         extend ActiveSupport::Concern
-        include Foreman::Controller::Parameters::KeepParam
 
         class_methods do
           def triggering_params_filter
             Foreman::ParameterFilter.new(::ForemanTasks::Triggering).tap do |filter|
-              filter.permit_by_context(:mode, :start_at, :start_before,
-                                       *::ForemanTasks::Triggering::PARAMS,
-                                       :nested => true)
+              filter.permit_by_context(
+                [
+                  :mode,
+                  :start_at,
+                  :start_before,
+                  *::ForemanTasks::Triggering::PARAMS,
+                  :days_of_week => {},
+                  :time => {},
+                  :end_time => {}
+                ],
+                :nested => true
+              )
             end
           end
         end
 
         def triggering_params
-          keep_param(params, :triggering, :days_of_week, :time, :end_time) do
-            self.class.triggering_params_filter.filter_params(params, parameter_filter_context, :triggering)
-          end
+          self.class.triggering_params_filter.filter_params(params, parameter_filter_context, :triggering)
         end
       end
     end
