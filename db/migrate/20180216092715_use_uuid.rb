a90ab057dd467bbcc6ee081997e7cdc237850db6
Adam Ruzicka <aruzicka@redhat.com>
2018-02-16 10:32:50 +0100

    Fixes #22602 - Use UUID column type for UUIDs on PostgreSQL

---
+++ db/migrate/20180216092715_use_uuid.rb
@@ -0,0 +1,44 @@
+class UseUuid < ActiveRecord::Migration[5.0]
+  # PostgreSQL has a special column type for storing UUIDs.
+  # Using this type instead of generic string should lead to having
+  #  smaller DB and possibly better overall performance.
+  def up
+    if on_postgresql?
+      change_table :foreman_tasks_tasks do |t|
+        t.change :id, :uuid, :using => 'id::uuid'
+        t.change :parent_task_id, :uuid, :using => 'parent_task_id::uuid'
+      end
+
+      change_table :foreman_tasks_task_group_members do |t|
+        t.change :task_id, :uuid, :using => 'task_id::uuid'
+      end
+
+      change_table :foreman_tasks_locks do |t|
+        t.change :task_id, :uuid, :using => 'task_id::uuid'
+      end
+    end
+  end
+
+  def down
+    if on_postgresql?
+      change_table :foreman_tasks_tasks do |t|
+        t.change :id, :string
+        t.change :parent_task_id, :string
+      end
+
+      change_table :foreman_tasks_task_group_members do |t|
+        t.change :task_id, :string
+      end
+
+      change_table :foreman_tasks_locks do |t|
+        t.change :task_id, :string
+      end
+    end
+  end
+
+  private
+
+  def on_postgresql?
+    ActiveRecord::Base.connection.instance_of?(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter)
+  end
+end
