df6247eeb35872bb2e54d69a4897ff8edee6c275
Adam Ruzicka <aruzicka@redhat.com>
2018-05-16 16:05:55 +0200

    Fixes #22657 - Make rubocop happy

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -16,18 +16,18 @@ module ForemanTasks
       describe 'taxonomy scoping' do
         let(:organizations) { (0..1).map { FactoryBot.create(:organization) } }
         let(:linked_task) do
-          ->(resource) do
+          lambda do |resource|
             t = FactoryBot.create(:some_task)
             ForemanTasks::Lock.link!(resource, t.id)
             t
           end
         end
-        let(:tasks) { organizations.map { |o| linked_task.(o) } + [FactoryBot.create(:some_task)] }
+        let(:tasks) { organizations.map { |o| linked_task.call(o) } + [FactoryBot.create(:some_task)] }
         let(:in_taxonomy_scope) do
-          ->(organization, location = nil, &block) do
+          lambda do |organization, location = nil, &block|
             Organization.current = organization unless organization.nil?
             Location.current = location unless location.nil?
-            yield organization, location
+            block.call organization, location
             Organization.current = Location.current = nil
           end
         end
@@ -35,14 +35,15 @@ module ForemanTasks
         it 'takes other searches into account' do
           task = tasks.first
           @controller.stubs(:params).returns(:search => "id = #{task.id}")
-          in_taxonomy_scope.(organizations.first) do |_o, _l|
+          in_taxonomy_scope.call(organizations.first) do |_o, _l|
             results = @controller.send(:filter, ForemanTasks::Task)
             results.map(&:id).sort.must_equal [task.id]
           end
         end
 
         it 'does not scope by taxonomy if unset' do
-          organizations; tasks
+          organizations
+          tasks
           @controller.send(:current_taxonomy_search).must_equal ''
           results = @controller.send(:filter, ForemanTasks::Task)
           results.map(&:id).sort.must_equal tasks.map(&:id).sort
@@ -50,7 +51,7 @@ module ForemanTasks
 
         it 'scopes by organization if set' do
           scoped, _, unscoped = tasks
-          in_taxonomy_scope.(organizations.first) do |o, _l|
+          in_taxonomy_scope.call(organizations.first) do |o, _l|
             @controller.send(:current_taxonomy_search).must_equal "(organization_id = #{o.id})"
             results = @controller.send(:filter, ForemanTasks::Task)
             results.map(&:id).sort.must_equal [scoped, unscoped].map(&:id).sort
@@ -58,7 +59,7 @@ module ForemanTasks
         end
 
         it 'scopes by org and location if set' do
-          in_taxonomy_scope.(organizations.first, FactoryBot.create(:location)) do |o, l|
+          in_taxonomy_scope.call(organizations.first, FactoryBot.create(:location)) do |o, l|
             @controller.send(:current_taxonomy_search).must_equal "(organization_id = #{o.id} AND location_id = #{l.id})"
           end
         end
