419b2923efa02fceb23c7cfa6397808896e9af8c
Ivan Nečas <inecas@redhat.com>
2018-09-10 17:52:08 +0200

    Fixes #24872 - fix import_facts deprecation

--- app/lib/actions/foreman/host/import_facts.rb
+++ app/lib/actions/foreman/host/import_facts.rb
@@ -9,13 +9,13 @@ module Actions
         def plan(_host_type, host_name, facts, certname, proxy_id)
           facts['domain'].try(:downcase!)
           host = if SETTINGS[:version].short > '1.16'
-                   ::Host::Base.import_host(host_name, certname, proxy_id)
+                   ::Host::Base.import_host(host_name, certname)
                  else
                    # backwards compatibility
                    ::Host::Managed.import_host(host_name, facts['_type'], certname, proxy_id)
                  end
           host.save(:validate => false) if host.new_record?
-          action_subject(host, :facts => facts.to_unsafe_h)
+          action_subject(host, :facts => facts.to_unsafe_h, :proxy_id => proxy_id)
           if host.build?
             ::Foreman::Logging.logger('foreman-tasks').info "Skipping importing of facts for #{host.name} because it's in build mode"
           else
@@ -26,7 +26,7 @@ module Actions
         def run
           ::User.as :admin do
             host           = ::Host.find(input[:host][:id])
-            state          = host.import_facts(input[:facts])
+            state          = host.import_facts(input[:facts], proxy)
             output[:state] = state
           end
         rescue ::Foreman::Exception => e
@@ -34,6 +34,10 @@ module Actions
           # the Host is in the build state. This can be refactored once
           # issue #3959 is fixed.
           raise e unless e.code == 'ERF51-9911'
+        end
+
+        def proxy
+          SmartProxy.find_by(id: input[:proxy_id]) if input[:proxy_id].present?
         end
 
         def rescue_strategy
