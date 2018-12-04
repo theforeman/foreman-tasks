8e5f404a74f6602f2ec529b43d654bec720e9924
Ivan Nečas <inecas@redhat.com>
2018-12-04 09:36:45 +0100

    Fix intermittent task controller failures
    By waiting for specific event to happen

--- test/foreman_tasks_test_helper.rb
+++ test/foreman_tasks_test_helper.rb
@@ -12,3 +12,14 @@ FactoryBot.find_definitions
 
 ForemanTasks.dynflow.require!
 ForemanTasks.dynflow.config.disable_active_record_actions = true
+
+# waits for the passed block to return non-nil value and reiterates it while getting false
+# (till some reasonable timeout). Useful for forcing the tests for some event to occur
+def wait_for(waiting_message = 'something to happen')
+  30.times do
+    ret = yield
+    return ret if ret
+    sleep 0.3
+  end
+  raise "waiting for #{waiting_message} was not successful"
+end
