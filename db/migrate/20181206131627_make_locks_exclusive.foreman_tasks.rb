07aa313b930f56a04146f2eda02c36826faae7af
Adam Ruzicka <aruzicka@redhat.com>
2018-12-08 16:13:17 +0100

    Fixes #25608 - Fix migrations on sqlite and mysql

--- db/migrate/20181206131627_make_locks_exclusive.foreman_tasks.rb
+++ db/migrate/20181206131627_make_locks_exclusive.foreman_tasks.rb
@@ -3,7 +3,7 @@ class MakeLocksExclusive < ActiveRecord::Migration[5.0]
     change_table :foreman_tasks_locks do |t|
       t.remove :exclusive
       t.remove :name
-      t.remove_index [:resource_type, :resource_id]
+      t.remove_index :name => 'index_foreman_tasks_locks_on_resource_type_and_resource_id'
       t.index [:resource_type, :resource_id], :unique => true
     end
   end
