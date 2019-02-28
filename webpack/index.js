07c75c2df48d2f99e166308097c39a4bc043d02e
laviro <1ronlavi@gmail.com>
2019-02-28 23:23:38 +0200

    Fixes #26073 - fix eslint error

--- webpack/index.js
+++ webpack/index.js
@@ -1,3 +1,7 @@
+/* eslint import/no-unresolved: [2, { ignore: [foremanReact/*] }] */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
 import componentRegistry from 'foremanReact/components/componentRegistry';
 import ForemanTasks from './ForemanTasks';
 
