a040206ee86a8cbb7085d1cd30babd01cb36bc7c
Ivan Nečas <inecas@redhat.com>
2019-02-28 17:49:27 +0100

    Fixes #26073 - add authorization to ex_tasks
    No reason for skipping it there. Also removing additional unneeded code.

--- app/controllers/foreman_tasks/react_controller.rb
+++ app/controllers/foreman_tasks/react_controller.rb
@@ -1,11 +1,17 @@
 module ForemanTasks
   class ReactController < ::ApplicationController
-    skip_before_action :authorize
-
-    include Rails.application.routes.url_helpers
-
     def index
       render 'foreman_tasks/layouts/react', :layout => false
+    end
+
+    private
+
+    def controller_permission
+      :foreman_tasks
+    end
+
+    def action_permission
+      :view
     end
   end
 end
