c4f4adfb4a71439ed3c1a2fa531fee2bd38c6a18
Ivan Nečas <inecas@redhat.com>
2019-04-03 18:38:16 +0200

    Refs #26527 - dry tests

--- test/unit/summarizer_test.rb
+++ test/unit/summarizer_test.rb
@@ -7,7 +7,8 @@ class SummarizerTest < ActiveSupport::TestCase
 
   describe ForemanTasks::Task::Summarizer do
     before do
-      build_tasks
+      @tasks_builder = HistoryTasksBuilder.new
+      @tasks_builder.build
     end
 
     let :subject do
@@ -15,13 +16,7 @@ class SummarizerTest < ActiveSupport::TestCase
     end
 
     let :expected do
-      { 'running' => { recent: 3, total: 6 },
-        'stopped' => { recent: 3, total: 7,
-                       by_result: {
-                         'success' => { recent: 2, total: 4 },
-                         'warning' => { recent: 1, total: 2 },
-                         'error' => { recent: 0, total: 1 }
-                       } } }
+      @tasks_builder.distribution
     end
 
     it 'is able to group tasks counts by state and result' do
@@ -39,37 +34,6 @@ class SummarizerTest < ActiveSupport::TestCase
         assert_equal expected_summary[key], summary[key],
                      "#{value_desc}[#{key}] expected to be #{expected_summary[key]}, was #{summary[key]}"
       end
-    end
-
-    # rubocop:disable Performance/TimesMap
-    def build_tasks
-      expected.each do |(state, status_summary)|
-        tasks = status_summary[:total].times.map do
-          ForemanTasks::Task.create(type: ForemanTasks::Task.name,
-                                    label: 'test',
-                                    state: state,
-                                    result: 'pending').tap do |task|
-            task.update(state_updated_at: nil)
-          end
-        end
-        recent_tasks = tasks.take(status_summary[:recent])
-        recent_tasks.each { |t| t.update(state_updated_at: Time.now.utc) }
-
-        untouched_recent_tasks = recent_tasks
-        untouched_old_tasks = tasks - recent_tasks
-        status_summary.fetch(:by_result, {}).each do |(result, result_summary)|
-          result_summary[:recent].times do |_i|
-            task = untouched_recent_tasks.shift
-            task.update(result: result)
-          end
-
-          (result_summary[:total] - result_summary[:recent]).times do |_i|
-            task = untouched_old_tasks.shift
-            task.update(result: result)
-          end
-        end
-      end
-      # rubocop:enable Performance/TimesMap
     end
   end
 end
