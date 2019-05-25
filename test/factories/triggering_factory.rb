24f1a4d22407c6294810c574763b4e8d43761740
Michael Moll <kvedulv@kvedulv.de>
2019-05-25 14:24:50 +0200

    wip

--- test/factories/triggering_factory.rb
+++ test/factories/triggering_factory.rb
@@ -4,21 +4,21 @@ FactoryBot.define do
 
     trait :future do
       time = Time.zone.now
-      mode :future
-      start_at time
+      mode { :future }
+      start_at { time }
       start_at_raw(time.strftime(ForemanTasks::Triggering::TIME_FORMAT))
     end
 
     trait :recurring do
-      mode :recurring
-      input_type :cronline
-      cronline '* * * * *'
+      mode { :recurring }
+      input_type { :cronline }
+      cronline { '* * * * *' }
       after(:build) { |triggering| triggering.recurring_logic = build(:recurring_logic) }
     end
 
     trait :end_time_limited do
-      end_time_limited true
-      end_time(Time.zone.now + 60)
+      end_time_limited { true }
+      end_time { Time.zone.now + 60 }
     end
   end
 end
