253162e84ccad504e110cb30479bb09325d9e4ff
Ivan Nečas <inecas@redhat.com>
2019-07-12 17:22:43 +0200

    Fixes #27292 - workaround for c3 destroy issue
    There is a known issue in react-c3js that can lead to the charts to
    disappear due to some race condition during component unmounting/destroy
    phase https://github.com/bcbcarl/react-c3js/issues/22.
    
    Delaying the actual destroy a bit seems to help with workarounding the
    issue.

--- webpack/ForemanTasks/Components/Chart/Chart.js
+++ webpack/ForemanTasks/Components/Chart/Chart.js
@@ -38,7 +38,12 @@ class C3Chart extends React.Component {
 
   destroyChart() {
     try {
-      this.chart = this.chart.destroy();
+      // A workaround for a case, where the chart might be still in transition
+      // phase while unmounting/destroying - destroying right away leads
+      // to issue described in https://github.com/bcbcarl/react-c3js/issues/22.
+      // Delaying the destroy a bit seems to resolve the issue.
+      setTimeout(this.chart.destroy, 1000);
+      this.chart = null;
     } catch (err) {
       throw new Error('Internal C3 error', err);
     }
