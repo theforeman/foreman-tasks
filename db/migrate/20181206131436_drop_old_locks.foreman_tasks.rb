6d7a8e328a745ebc875d3d41ffb7576b6bf18c9d
Adam Ruzicka <aruzicka@redhat.com>
2019-07-17 14:00:16 +0200

    Fixes #25608 - Prune duplicate locks

--- db/migrate/20181206131436_drop_old_locks.foreman_tasks.rb
+++ db/migrate/20181206131436_drop_old_locks.foreman_tasks.rb
@@ -7,6 +7,15 @@ class DropOldLocks < ActiveRecord::Migration[5.0]
     scope = scope.where(:exclusive => false)
                  .or(scope.where("#{ForemanTasks::Task.table_name}.state" => ['stopped', nil]))
     scope.limit(BATCH_SIZE).delete_all while scope.any?
+
+    # For each group of locks, where each lock has the same task_id, resource_type and resource_id
+    # return the highest id within the group, if there is more than 1 lock in the group
+    scope = ForemanTasks::Lock.select("MAX(id) as id")
+                              .group(:task_id, :resource_type, :resource_id)
+                              .having("count(*) > 1")
+
+    # Make sure there is at most one lock per task and resource
+    ForemanTasks::Lock.where(:id => scope.limit(BATCH_SIZE)).delete_all while scope.any?
   end
 
   def down
