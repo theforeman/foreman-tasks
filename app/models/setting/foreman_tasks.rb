3b7f55b8b24f55a888febcf10d82065adf2d72e0
Ondřej Ezr <oezr@redhat.com>
2019-09-10 17:48:33 +0200

    Fixes #27815 - setting initialization improvement

--- app/models/setting/foreman_tasks.rb
+++ app/models/setting/foreman_tasks.rb
@@ -1,28 +1,24 @@
 class Setting::ForemanTasks < Setting
+  def self.default_settings
+    [
+      set('foreman_tasks_sync_task_timeout', N_('Number of seconds to wait for synchronous task to finish.'), 120),
+      set('dynflow_allow_dangerous_actions', N_('Allow unlocking actions which can have dangerous consequences.'), false),
+      set('dynflow_enable_console', N_('Enable the dynflow console (/foreman_tasks/dynflow) for debugging'), true),
+      set('dynflow_console_require_auth', N_('Require user to be authenticated as user with admin rights when accessing dynflow console'), true),
+      set('foreman_tasks_proxy_action_retry_count', N_('Number of attempts to start a task on the smart proxy before failing'), 4),
+      set('foreman_tasks_proxy_action_retry_interval', N_('Time in seconds between retries'), 15),
+      set('foreman_tasks_proxy_batch_trigger', N_('Allow triggering tasks on the smart proxy in batches'), true),
+      set('foreman_tasks_proxy_batch_size', N_('Number of tasks which should be sent to the smart proxy in one request, if foreman_tasks_proxy_batch_trigger is enabled'), 100),
+      set('foreman_tasks_troubleshooting_url',
+          N_('Url pointing to the task troubleshooting documentation. '\
+             'It should contain %{label} placeholder, that will be replaced with normalized task label '\
+             '(restricted to only alphanumeric characters)). %{version} placeholder is also available.'),
+          nil)
+    ]
+  end
+
   def self.load_defaults
-    # Check the table exists
-    return unless super
-
     Setting::BLANK_ATTRS.push('foreman_tasks_troubleshooting_url')
-
-    transaction do
-      [
-        set('foreman_tasks_sync_task_timeout', N_('Number of seconds to wait for synchronous task to finish.'), 120),
-        set('dynflow_allow_dangerous_actions', N_('Allow unlocking actions which can have dangerous consequences.'), false),
-        set('dynflow_enable_console', N_('Enable the dynflow console (/foreman_tasks/dynflow) for debugging'), true),
-        set('dynflow_console_require_auth', N_('Require user to be authenticated as user with admin rights when accessing dynflow console'), true),
-        set('foreman_tasks_proxy_action_retry_count', N_('Number of attempts to start a task on the smart proxy before failing'), 4),
-        set('foreman_tasks_proxy_action_retry_interval', N_('Time in seconds between retries'), 15),
-        set('foreman_tasks_proxy_batch_trigger', N_('Allow triggering tasks on the smart proxy in batches'), true),
-        set('foreman_tasks_proxy_batch_size', N_('Number of tasks which should be sent to the smart proxy in one request, if foreman_tasks_proxy_batch_trigger is enabled'), 100),
-        set('foreman_tasks_troubleshooting_url',
-            N_('Url pointing to the task troubleshooting documentation. '\
-               'It should contain %{label} placeholder, that will be replaced with normalized task label '\
-               '(restricted to only alphanumeric characters)). %{version} placeholder is also available.'),
-            nil)
-      ].each { |s| create! s.update(:category => 'Setting::ForemanTasks') }
-    end
-
-    true
+    super
   end
 end
