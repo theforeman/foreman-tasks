20e2928d9d08be4589ee9e419bbf15c8b67070a0
MariaAga <mariaagaphonchev@gmail.com>
2019-09-26 16:25:14 +0300

    Fixes #27683 - tasks details should use the core date (#453)

--- webpack/ForemanTasks/Components/TaskDetails/Components/TaskHelper.js
+++ webpack/ForemanTasks/Components/TaskDetails/Components/TaskHelper.js
@@ -1,20 +1,6 @@
 import { translate as __, documentLocale } from 'foremanReact/common/I18n';
-import React from 'react';
-import { FormattedRelative } from 'react-intl';
+import { isoCompatibleDate } from 'foremanReact/common/helpers';
 import humanizeDuration from 'humanize-duration';
-
-const formatDate = date => {
-  // Firefox doesnt format dd-mm-yyyy type strings to date
-  if (typeof date === 'string' || date instanceof String)
-    return new Date(date.replace(/-/, '/').replace(/-/, '/'));
-  return date;
-};
-
-export const timeInWords = time => {
-  if (!time) return __('N/A');
-  time = formatDate(time);
-  return <FormattedRelative value={time} />;
-};
 
 export const durationInWords = (
   start,
@@ -22,8 +8,8 @@ export const durationInWords = (
   selectedLocale = documentLocale()
 ) => {
   if (!start) return __('N/A');
-  start = formatDate(start).getTime();
-  finish = formatDate(finish).getTime();
+  start = new Date(isoCompatibleDate(start)).getTime();
+  finish = new Date(isoCompatibleDate(finish)).getTime();
   return {
     text: humanizeDuration(new Date(finish - start).getTime(), {
       largest: 1,
