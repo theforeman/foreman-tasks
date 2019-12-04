accdcaaea032ed29d2148f107b2b0925c50b2edc
Adam Ruzicka <aruzicka@redhat.com>
2019-12-04 11:27:24 +0100

    Fixes #28415 - Fix PG::AmbiguousColumn error on tasks search
    The original error was
    'Action failed' error (ActiveRecord::StatementInvalid): PG::AmbiguousColumn:
    ERROR: ORDER BY "started_at" is ambiguous | LINE 1: ...,4))

--- app/controllers/foreman_tasks/api/tasks_controller.rb
+++ app/controllers/foreman_tasks/api/tasks_controller.rb
@@ -240,7 +240,7 @@ module ForemanTasks
         sort_by = ordering_params[:sort_by] || 'started_at'
         sort_order = ordering_params[:sort_order] || 'DESC'
         scope = scope.select("*, coalesce(ended_at, current_timestamp) - coalesce(coalesce(started_at, ended_at), current_timestamp) as duration")
-        scope.order("#{sort_by} #{sort_order}")
+        scope.order(sort_by => sort_order)
       end
 
       def task_hash(task)
