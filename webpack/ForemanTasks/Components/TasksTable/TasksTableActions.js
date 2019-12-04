5d2d5b41c7f13a03c7548664109e92f08b73f172
MariaAga <mariaagaphonchev@gmail.com>
2019-12-04 15:42:20 +0200

    Fixes #28416 - use bulk resume api in tasks table button

--- webpack/ForemanTasks/Components/TasksTable/TasksTableActions.js
+++ webpack/ForemanTasks/Components/TasksTable/TasksTableActions.js
@@ -2,15 +2,17 @@ import { getURIQuery } from 'foremanReact/common/helpers';
 import { getTableItemsAction } from 'foremanReact/components/common/table';
 import API from 'foremanReact/API';
 import { addToast } from 'foremanReact/redux/actions/toasts';
-import { translate as __ } from 'foremanReact/common/I18n';
+import { translate as __, sprintf } from 'foremanReact/common/I18n';
+import URI from 'urijs';
 import {
   TASKS_TABLE_ID,
   SELECT_ROWS,
   UNSELECT_ALL_ROWS,
   UNSELECT_ROWS,
-  RESUME,
-  CANCEL,
   UPDATE_CLICKED,
+  TASKS_RESUME_REQUEST,
+  TASKS_RESUME_SUCCESS,
+  TASKS_RESUME_FAILURE,
 } from './TasksTableConstants';
 import { getApiPathname } from './TasksTableHelpers';
 import { fetchTasksSummary } from '../TasksDashboard/TasksDashboardActions';
@@ -18,7 +20,12 @@ import { fetchTasksSummary } from '../TasksDashboard/TasksDashboardActions';
 export const getTableItems = url =>
   getTableItemsAction(TASKS_TABLE_ID, getURIQuery(url), getApiPathname(url));
 
-export const cancelTask = (id, name, url, parentTaskID) => async dispatch => {
+export const cancelTask = ({
+  id,
+  name,
+  url,
+  parentTaskID,
+}) => async dispatch => {
   await dispatch(cancelTaskRequest(id, name));
   dispatch(getTableItems(url));
   dispatch(fetchTasksSummary(getURIQuery(url).time, parentTaskID));
@@ -49,7 +56,12 @@ export const cancelTaskRequest = (id, name) => async dispatch => {
   }
 };
 
-export const resumeTask = (id, name, url, parentTaskID) => async dispatch => {
+export const resumeTask = ({
+  id,
+  name,
+  url,
+  parentTaskID,
+}) => async dispatch => {
   await dispatch(resumeTaskRequest(id, name));
   dispatch(getTableItems(url));
   dispatch(fetchTasksSummary(getURIQuery(url).time), parentTaskID);
@@ -93,34 +105,88 @@ export const unselectRow = id => ({
   payload: id,
 });
 
-export const actionSelected = (
-  actionType,
+export const bulkResumeRequest = resumeTasks => {
+  const ids = resumeTasks.map(task => task.id);
+  const url = new URI('/foreman_tasks/api/tasks/bulk_resume');
+  url.setSearch('task_ids[]', ids);
+  return API.post(url);
+};
+
+export const bulkResume = ({
   selected,
   url,
-  parentTaskID
-) => async dispatch => {
+  parentTaskID,
+}) => async dispatch => {
+  const resumeTasks = selected.filter(task => task.isResumable);
+  if (resumeTasks.length < selected.length)
+    dispatch(
+      addToast({
+        type: 'warning',
+        message: __('Not all the selected tasks can be resumed'),
+      })
+    );
+  if (resumeTasks.length) {
+    try {
+      dispatch({ type: TASKS_RESUME_REQUEST });
+      const { data } = await bulkResumeRequest(resumeTasks);
+      dispatch({ type: TASKS_RESUME_SUCCESS });
+      const toastInfo = {
+        resumed: { type: 'success', text: 'was resumed' },
+        failed: { type: 'error', text: 'could not be resumed' },
+      };
+      const toastDispatch = (type, name) =>
+        dispatch(
+          addToast({
+            type: toastInfo[type].type,
+            message: sprintf(__('%(name)s Task execution %(type)s'), {
+              name,
+              type: toastInfo[type].text,
+            }),
+          })
+        );
+
+      ['resumed', 'failed'].forEach(type => {
+        data[type].forEach(task => {
+          toastDispatch(type, task.action);
+        });
+      });
+      if (data.resumed.length) {
+        dispatch(getTableItems(url));
+        dispatch(fetchTasksSummary(getURIQuery(url).time, parentTaskID));
+      }
+    } catch (error) {
+      dispatch({ type: TASKS_RESUME_FAILURE, error });
+      dispatch(
+        addToast({
+          type: 'error',
+          message: `${__(`Cannot resume tasks at the moment`)} ${error}`,
+        })
+      );
+    }
+  }
+};
+
+export const bulkCancel = ({
+  selected,
+  url,
+  parentTaskID,
+}) => async dispatch => {
   let notAllActionable = false;
   let someActionable = false;
   const promises = selected.map(task => {
-    if (actionType === RESUME && task.isResumeble) {
+    if (task.isCancellable) {
       someActionable = true;
-      return dispatch(resumeTaskRequest(task.id, task.name, url));
-    } else if (actionType === CANCEL && task.isCancelleble) {
-      someActionable = true;
-      return dispatch(cancelTaskRequest(task.id, task.name, url));
+      return dispatch(cancelTaskRequest(task.id, task.name));
     }
     notAllActionable = true;
     return null;
   });
+
   if (notAllActionable)
     dispatch(
       addToast({
         type: 'warning',
-        message: __(
-          `Not all the selected tasks can be ${
-            actionType === RESUME ? 'resumed' : 'canceled'
-          }`
-        ),
+        message: __('Not all the selected tasks can be canceled'),
       })
     );
   if (someActionable) {
