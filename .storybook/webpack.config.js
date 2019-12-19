74ee3878b7e780cef056066293be5846d6999826
Avi Sharvit <sharvita@gmail.com>
2019-12-19 10:21:16 +0200

    Fixes #28243 - upgrade vendor to v3 (#474)

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,3 +1,4 @@
+const babelOptions = require('../.babelrc.js');
 let path = require('path');
 
 module.exports = {
@@ -7,22 +8,11 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
-        options: {
-          presets: [
-            path.join(__dirname, '..', 'node_modules/babel-preset-react'),
-            path.join(__dirname, '..', 'node_modules/babel-preset-env')
-          ],
-          plugins: [
-            path.join(__dirname, '..', 'node_modules/babel-plugin-transform-class-properties'),
-            path.join(__dirname, '..', 'node_modules/babel-plugin-transform-object-rest-spread'),
-            path.join(__dirname, '..', 'node_modules/babel-plugin-transform-object-assign'),
-            path.join(__dirname, '..', 'node_modules/babel-plugin-syntax-dynamic-import')
-          ]
-        }
+        options: babelOptions,
       },
       {
         test: /(\.png|\.gif)$/,
-        loader: 'url-loader?limit=32767'
+        loader: 'url-loader?limit=32767',
       },
       {
         test: /\.css$/,
@@ -30,30 +20,61 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        loaders: ['style-loader', 'css-loader', {
-          loader: 'sass-loader',
-          options: {
-            includePaths: [
-              // teach webpack to resolve patternfly dependencies
-              path.resolve(__dirname, '..', 'node_modules', 'patternfly', 'dist', 'sass'),
-              path.resolve(__dirname, '..', 'node_modules', 'bootstrap-sass', 'assets', 'stylesheets'),
-              path.resolve(__dirname, '..', 'node_modules', 'font-awesome-sass', 'assets', 'stylesheets')
-            ]
-          }
-        }]
+        loaders: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              includePaths: [
+                // teach webpack to resolve patternfly dependencies
+                path.resolve(
+                  __dirname,
+                  '..',
+                  'node_modules',
+                  'patternfly',
+                  'dist',
+                  'sass'
+                ),
+                path.resolve(
+                  __dirname,
+                  '..',
+                  'node_modules',
+                  'bootstrap-sass',
+                  'assets',
+                  'stylesheets'
+                ),
+                path.resolve(
+                  __dirname,
+                  '..',
+                  'node_modules',
+                  'font-awesome-sass',
+                  'assets',
+                  'stylesheets'
+                ),
+              ],
+            },
+          },
+        ],
       },
       {
         test: /\.md$/,
-        loaders: ['raw-loader']
+        loaders: ['raw-loader'],
       },
       {
         test: /(\.ttf|\.woff|\.woff2|\.eot|\.svg|\.jpg)$/,
-        loaders: ['url-loader']
+        loaders: ['url-loader'],
       },
     ],
   },
 
   resolve: {
+    alias: {
+      foremanReact: path.join(
+        __dirname,
+        '../../foreman/webpack/assets/javascripts/react_app'
+      ),
+    },
     modules: [
       path.join(__dirname, '..', 'webpack'),
       path.join(__dirname, '..', 'node_modules'),
