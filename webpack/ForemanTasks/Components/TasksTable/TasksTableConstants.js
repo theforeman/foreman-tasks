bead1be153446bf66d560c75cc8069189c5a77a0
Adam Růžička <adamruzicka@users.noreply.github.com>
2020-02-26 16:04:14 +0100

    Fixes #29003 - Fix controller path for task search bookmarks (#503)
    * Fixes #29003 - Fix controller path for task search bookmarks
    
    * Fixes #29003 - Remove unused constants
    
    * Fixes #29003 - Use separate controllers for autocomplete and tasks
    
    (cherry picked from commit f9d8babee4e48bf731cea6f7194eafcd7c1a3ca3)

--- webpack/ForemanTasks/Components/TasksTable/TasksTableConstants.js
+++ webpack/ForemanTasks/Components/TasksTable/TasksTableConstants.js
@@ -1,10 +1,6 @@
 import { getControllerSearchProps } from 'foremanReact/constants';
 
 export const TASKS_TABLE_ID = 'TASKS_TABLE';
-export const TASKS_CONTROLLER = 'tasks';
-export const TASKS_SUCCESS = 'TASKS_SUCCESS';
-export const TASKS_REQUEST = 'TASKS_REQUEST';
-export const TASKS_FAILURE = 'TASKS_FAILURE';
 export const SELECT_ROWS = 'SELECT_ROWS';
 export const UNSELECT_ROWS = 'UNSELECT_ROWS';
 export const UNSELECT_ALL_ROWS = 'UNSELECT_ALL_ROWS';
@@ -13,4 +9,7 @@ export const RESUME = 'RESUME';
 export const CLOSED = 'CLOSED';
 
 export const TASKS_TABLE_SELECTED_MODAL = 'TASKS_TABLE_SELECTED_MODAL';
-export const TASKS_SEARCH_PROPS = getControllerSearchProps('tasks');
+export const TASKS_SEARCH_PROPS = {
+  ...getControllerSearchProps('tasks'),
+  controller: 'foreman_tasks/tasks'
+};
