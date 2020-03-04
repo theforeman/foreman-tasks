4eaec65e64ca1f4cc764c1b2aeee48a23b2f8790
MariaAga <mariaagaphonchev@gmail.com>
2020-03-04 11:09:27 +0200

    Fixes #28246 - Create bulk cancel api

--- config/routes.rb
+++ config/routes.rb
@@ -55,6 +55,7 @@ Foreman::Application.routes.draw do
         collection do
           post :bulk_search
           post :bulk_resume
+          post :bulk_cancel
           get :summary
           get '/summary/:id/sub_tasks/', action: 'summary_sub_tasks'
           post :callback
