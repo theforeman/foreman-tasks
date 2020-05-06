eacb8a48a773444e05b35878842c764747218e5c
Adam Ruzicka <aruzicka@redhat.com>
2020-05-06 12:46:13 +0200

    Fixes #29423 - Provide fallback value in case the setting is unset

--- app/lib/foreman_tasks/concerns/polling_action_extensions.rb
+++ app/lib/foreman_tasks/concerns/polling_action_extensions.rb
@@ -2,7 +2,7 @@ module ForemanTasks
   module Concerns
     module PollingActionExtensions
       def poll_intervals
-        multiplier = Setting[:foreman_tasks_polling_multiplier]
+        multiplier = Setting[:foreman_tasks_polling_multiplier] || 1
 
         # Prevent the intervals from going below 0.5 seconds
         super.map { |interval| [interval * multiplier, 0.5].max }
