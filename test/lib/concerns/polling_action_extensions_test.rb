18d292cc6e951a452b2418a7d67cd03d8f1f4ac4
Adam Ruzicka <aruzicka@redhat.com>
2020-05-06 11:21:31 +0200

    Fixes #29423 - Make rubocop happy

--- test/lib/concerns/polling_action_extensions_test.rb
+++ test/lib/concerns/polling_action_extensions_test.rb
@@ -20,13 +20,13 @@ module ForemanTasks
 
         it 'cannot make intervals shorter than 0.5 seconds' do
           Setting.expects(:[]).with(:foreman_tasks_polling_multiplier).returns 0
-          _(Action.allocate.poll_intervals).must_equal default_intervals.map { 0.5 }
+          _(Action.allocate.poll_intervals).must_equal(default_intervals.map { 0.5 })
         end
 
         it 'can be used to make the intervals longer' do
           value = 5
           Setting.expects(:[]).with(:foreman_tasks_polling_multiplier).returns value
-          _(Action.allocate.poll_intervals).must_equal default_intervals.map { |i| i * value }
+          _(Action.allocate.poll_intervals).must_equal(default_intervals.map { |i| i * value })
         end
       end
     end
