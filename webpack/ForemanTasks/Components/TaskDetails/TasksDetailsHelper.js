2942b22dfb07860a69f03db2b0e599d5aa1cd002
Leos Stejskal <lstejska@redhat.com>
2020-05-11 11:22:38 +0200

    Fixes #29495 - Filter trailing forward slash from task URL

--- webpack/ForemanTasks/Components/TaskDetails/TasksDetailsHelper.js
+++ webpack/ForemanTasks/Components/TaskDetails/TasksDetailsHelper.js
@@ -1,1 +1,6 @@
-export const getTaskID = () => window.location.pathname.split('/').slice(-1)[0];
+// Get Task ID from URL. Split url by '/', filter non-empty values and get Task ID.
+export const getTaskID = () =>
+  window.location.pathname
+    .split('/')
+    .filter(i => i)
+    .slice(-1)[0];
--- webpack/ForemanTasks/Components/TaskDetails/__tests__/TaskDetails.test.js
+++ webpack/ForemanTasks/Components/TaskDetails/__tests__/TaskDetails.test.js
@@ -6,6 +6,11 @@ const fixtures = {
   'render without Props': {},
 };
 
+delete window.location;
+window.location = new URL(
+  'https://foreman.com/foreman_tasks/tasks/a15dd820-32f1-4ced-9ab7-c0fab8234c47/'
+);
+
 describe('TaskDetails', () => {
   describe('rendering', () =>
     testComponentSnapshotsWithFixtures(TaskDetails, fixtures));
