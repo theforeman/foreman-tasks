90a133745761767d443cc7cd7977575f3370745b
MariaAga <mariaagaphonchev@gmail.com>
2020-05-31 16:10:14 +0300

    Fixes #29966 - test fails in eslint for routes test

--- webpack/ForemanTasks/Routes/ForemanTasksRoutes.test.js
+++ webpack/ForemanTasks/Routes/ForemanTasksRoutes.test.js
@@ -6,7 +6,8 @@ import ForemanTasksRoutes from './ForemanTasksRoutes';
 describe('ForemanTasksRoutes', () => {
   it('should create routes', () => {
     Object.entries(ForemanTasksRoutes).forEach(([key, Route]) => {
-      const component = shallow(<Route.render history={{}} some="props" />);
+      const RouteRender = Route.render;
+      const component = shallow(<RouteRender history={{}} some="props" />);
       Route.renderResult = component;
     });
 
