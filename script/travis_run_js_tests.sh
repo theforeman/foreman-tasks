ab36cc636c2802923bc3d57c6af40b25081ce04e
laviro <1ronlavi@gmail.com>
2020-07-02 10:57:38 +0300

    Fixes #30274 - disabling npm cache on travis

--- script/travis_run_js_tests.sh
+++ script/travis_run_js_tests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -ev
 if [[ $( git diff --name-only HEAD~1..HEAD webpack/ .travis.yml babel.config.js .eslintrc package.json | wc -l ) -ne 0 ]]; then
+  npm ls @theforeman/test;
   npm run test;
   npm run publish-coverage;
   npm run lint;
