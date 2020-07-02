a901ae3daf20e8ef153946e139c59f21a4cb663d
Adam Ruzicka <aruzicka@redhat.com>
2020-07-02 15:41:49 +0200

    Fixes #30311 - Drop indexes only if they're present

--- db/migrate/20200611090846_add_task_lock_index_on_resource_type_and_task_id.rb
+++ db/migrate/20200611090846_add_task_lock_index_on_resource_type_and_task_id.rb
@@ -3,7 +3,7 @@ class AddTaskLockIndexOnResourceTypeAndTaskId < ActiveRecord::Migration[6.0]
     add_index :foreman_tasks_locks, [:task_id, :resource_type, :resource_id], name: 'index_tasks_locks_on_task_id_resource_type_and_resource_id'
     # These indexes are not needed as they can be gained from partial index lookups
     [:task_id, :name, :resource_type].each do |index|
-      remove_index :foreman_tasks_locks, index unless index_exists?(:foreman_tasks_locks, index)
+      remove_index :foreman_tasks_locks, index if index_exists?(:foreman_tasks_locks, index)
     end
   end
 end
