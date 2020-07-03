dece2266e395dad30298899d2d51b775740ddd82
Adam Ruzicka <aruzicka@redhat.com>
2020-07-03 10:17:44 +0200

    Refs #30311 - Change migration version to 5.2

--- db/migrate/20200611090846_add_task_lock_index_on_resource_type_and_task_id.rb
+++ db/migrate/20200611090846_add_task_lock_index_on_resource_type_and_task_id.rb
@@ -1,4 +1,4 @@
-class AddTaskLockIndexOnResourceTypeAndTaskId < ActiveRecord::Migration[6.0]
+class AddTaskLockIndexOnResourceTypeAndTaskId < ActiveRecord::Migration[5.2]
   def change
     add_index :foreman_tasks_locks, [:task_id, :resource_type, :resource_id], name: 'index_tasks_locks_on_task_id_resource_type_and_resource_id'
     # These indexes are not needed as they can be gained from partial index lookups
