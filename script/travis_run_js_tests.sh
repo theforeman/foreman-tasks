8aad5f175a3ee6c448a52aab7b2a1a58089d7cba
laviro <1ronlavi@gmail.com>
2020-07-09 17:50:17 +0300

    make travis work

--- script/travis_run_js_tests.sh
+++ script/travis_run_js_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ev
-if [[ $( git diff --name-only HEAD~1..HEAD webpack/ .travis.yml babel.config.js .eslintrc package.json | wc -l ) -ne 0 ]]; then
+if [[ $( git diff --name-only HEAD~1..HEAD webpack/ .travis.yml package.json | wc -l ) -ne 0 ]]; then
   npm run test;
   npm run publish-coverage;
   npm run lint;
