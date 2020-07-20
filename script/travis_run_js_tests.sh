5a6978b28bfb4d980f253fa188408ff13d8d36b8
Ondřej Ezr <oezr@redhat.com>
2020-07-20 10:13:33 +0200

    Drop Travis testing

--- script/travis_run_js_tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -ev
-if [[ $( git diff --name-only HEAD~1..HEAD webpack/ .travis.yml babel.config.js .eslintrc package.json | wc -l ) -ne 0 ]]; then
-  npm run test;
-  npm run publish-coverage;
-  npm run lint;
-fi
