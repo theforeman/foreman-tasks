b6abe49548d77829f06bc79a4505f6c725f69c27
MariaAga <mariaagaphonchev@gmail.com>
2020-12-13 19:37:26 +0200

    Fixes #31501 - Word Wrap for Long names in tasks table
    (cherry picked from commit 8f5cc534017dabc4289f6c8b1ce862cb2dcec6fc)

--- webpack/ForemanTasks/Components/TasksTable/formatters/actionNameCellFormatter.js
+++ webpack/ForemanTasks/Components/TasksTable/formatters/actionNameCellFormatter.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import { cellFormatter } from 'foremanReact/components/common/table';
+import EllipsisWithTooltip from 'react-ellipsis-with-tooltip';
 
 export const actionNameCellFormatter = url => (value, { rowData: { id } }) =>
-  cellFormatter(<a href={`/${url}/${id}`}>{value}</a>);
+  cellFormatter(
+    <a href={`/${url}/${id}`}>
+      <EllipsisWithTooltip>{value}</EllipsisWithTooltip>
+    </a>
+  );
