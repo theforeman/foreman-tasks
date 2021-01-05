2c7cf2cdaee74a51f736996785e3ee04cfde8212
Adam Ruzicka <aruzicka@redhat.com>
2021-01-05 13:28:32 +0100

    Define modules only if not in production

--- app/lib/actions/try.rb
+++ app/lib/actions/try.rb
@@ -1,19 +1,19 @@
-module Actions
-  module Try
-    # A set of actions to try different scenarios with tasks in development.
-    #
-    # Usage:
-    # from rails console, run:
-    #
-    #    # Successful task (ending in stopped state/success result)
-    #    ForemanTasks.sync_task(Actions::Try::Success)
-    #
-    #    # Failing task task (ending in paused state/error result)
-    #    ForemanTasks.sync_task(Actions::Try::Pause)
-    #
-    #    # Task that fails but resumes (ending in stopped state/warning result)
-    #    ForemanTasks.sync_task(Actions::Try::Skip)
-    unless Rails.env.production?
+unless Rails.env.production?
+  module Actions
+    module Try
+      # A set of actions to try different scenarios with tasks in development.
+      #
+      # Usage:
+      # from rails console, run:
+      #
+      #    # Successful task (ending in stopped state/success result)
+      #    ForemanTasks.sync_task(Actions::Try::Success)
+      #
+      #    # Failing task task (ending in paused state/error result)
+      #    ForemanTasks.sync_task(Actions::Try::Pause)
+      #
+      #    # Task that fails but resumes (ending in stopped state/warning result)
+      #    ForemanTasks.sync_task(Actions::Try::Skip)
       class Success < Actions::EntryAction
         def plan
           plan_self(hello: 'world')
