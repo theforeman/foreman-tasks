f0f9514460bfd7b60c63f942b281166e9bfd305a
MariaAga <mariaagaphonchev@gmail.com>
2021-01-07 19:28:06 +0200

    Refs #31501 - Word wrap for name in task details and table

--- webpack/ForemanTasks/Components/TasksTable/formatters/actionNameCellFormatter.js
+++ webpack/ForemanTasks/Components/TasksTable/formatters/actionNameCellFormatter.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { cellFormatter } from 'foremanReact/components/common/table';
-import EllipsisWithTooltip from 'react-ellipsis-with-tooltip';
 
 export const actionNameCellFormatter = url => (value, { rowData: { id } }) =>
   cellFormatter(
-    <a href={`/${url}/${id}`}>
-      <EllipsisWithTooltip>{value}</EllipsisWithTooltip>
+    <a href={`/${url}/${id}`} className="action-name-tasks-table">
+      {value}
     </a>
   );
