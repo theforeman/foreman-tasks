9ce07b2d2168a815099a5cfe65409e284981cfbf
Adam Ruzicka <aruzicka@redhat.com>
2021-06-04 11:32:30 +0200

    Fixes #32723 - Reduce the compatibility layer a bit

--- lib/foreman_tasks_core.rb
+++ lib/foreman_tasks_core.rb
@@ -1,30 +1,23 @@
 # The goal of ForemanTasksCore is to collect parts of foreman-tasks
 # that can be shared by the Foreman server and Foreman proxy
 #
-require 'foreman_tasks_core/runner'
+require 'smart_proxy_dynflow'
 require 'smart_proxy_dynflow/task_launcher'
-require 'smart_proxy_dynflow/ticker'
 require 'smart_proxy_dynflow/settings_loader'
+
+require 'foreman_tasks_core/runner'
 
 module ForemanTasksCore
   def self.dynflow_world
-    raise 'Dynflow world not set. Call initialize first' unless @dynflow_world
-    @dynflow_world
+    Proxy::Dynflow::Core.world
   end
 
   def self.dynflow_present?
-    defined? Dynflow
+    true
   end
 
-  def self.dynflow_setup(dynflow_world)
-    @dynflow_world = dynflow_world
-  end
-
-  def self.silent_dead_letter_matchers
-    [::Dynflow::DeadLetterSilencer::Matcher.new(ForemanTasksCore::Ticker)]
-  end
+  def self.dynflow_setup(_dynflow_world); end
 
   TaskLauncher = Proxy::Dynflow::TaskLauncher
-  Ticker = Proxy::Dynflow::Ticker
   SettingsLoader = Proxy::Dynflow::SettingsLoader
 end
