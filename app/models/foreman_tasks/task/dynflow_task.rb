a7077915b72a04c088a45f7386416ce91a8f8ca7
Adam Růžička <adamruzicka@users.noreply.github.com>
2021-06-21 16:22:07 +0200

    Fixes #32838 - Use AJ wrapper in case loading AJ fails (#639)
    * Fixes #32838 - Use AJ wrapper in case loading AJ fails
    
    This solves an edge case where an active job got turned into a proper
    dynflow action, but kept the same name. So after serialization and
    deserialization of the active job we got a dynflow action which has a
    different interface. This causes tasks index and detail to fail.
    
    (cherry picked from commit 18012ff1176355d408a1e35bd8e80017368ce4ba)

--- app/models/foreman_tasks/task/dynflow_task.rb
+++ app/models/foreman_tasks/task/dynflow_task.rb
@@ -140,12 +140,17 @@ module ForemanTasks
 
     def main_action
       return @main_action if defined?(@main_action)
+
+      @main_action = execution_plan && execution_plan.root_plan_step.try(:action, execution_plan)
       if active_job?
         job_data = active_job_data
-        @main_action = active_job_action(job_data['job_class'], job_data['arguments'])
-      else
-        @main_action = execution_plan && execution_plan.root_plan_step.try(:action, execution_plan)
+        begin
+          @main_action = active_job_action(job_data['job_class'], job_data['arguments'])
+        rescue => e
+          Foreman::Logging.exception("Failed to load ActiveJob for task #{id}", e, :logger => 'foreman-tasks')
+        end
       end
+      @main_action
     end
 
     # The class for ActiveJob jobs in Dynflow, JobWrapper is not expected to
