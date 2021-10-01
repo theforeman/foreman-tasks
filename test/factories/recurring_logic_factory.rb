51d8b31be88ddbe327d08c02506e716877a8a84e
Ondrej Prazak <oprazak@redhat.com>
2021-10-01 13:17:09 +0200

    Fixes #33612 - Add mutation for canceling recurring logics

--- test/factories/recurring_logic_factory.rb
+++ test/factories/recurring_logic_factory.rb
@@ -1,8 +1,14 @@
 FactoryBot.define do
   factory :recurring_logic, :class => ForemanTasks::RecurringLogic do
     cron_line { '* * * * *' }
-    after(:build) { |logic| logic.task_group = build(:recurring_logic_task_group) }
+    association :task_group
   end
 
+  factory :task_group, :class => ::ForemanTasks::TaskGroup do
+    type { "ForemanTasks::TaskGroups::RecurringLogicTaskGroup" }
+  end
   factory :recurring_logic_task_group, :class => ::ForemanTasks::TaskGroups::RecurringLogicTaskGroup
+  factory :task_group_member, :class => ::ForemanTasks::TaskGroupMember do
+    association :task_group, :task
+  end
 end
