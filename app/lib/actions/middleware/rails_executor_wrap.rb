0b8d9e0d4fa74ff786cd7d4468fe7143a52ad699
Adam Ruzicka <aruzicka@redhat.com>
2022-01-10 11:51:42 +0100

    Fixes #34227 - Reference Rails as ::Rails

--- app/lib/actions/middleware/rails_executor_wrap.rb
+++ app/lib/actions/middleware/rails_executor_wrap.rb
@@ -10,13 +10,13 @@ module Actions
     # for more details.
     class RailsExecutorWrap < Dynflow::Middleware
       def run(*args)
-        Rails.application.executor.wrap do
+        ::Rails.application.executor.wrap do
           pass(*args)
         end
       end
 
       def finalize
-        Rails.application.executor.wrap do
+        ::Rails.application.executor.wrap do
           pass
         end
       end
