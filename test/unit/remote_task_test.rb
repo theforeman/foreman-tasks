52b001e44038f2a3256ff63879b88248774e7536
Adam Ruzicka <a.ruzicka@outlook.com>
2022-03-03 16:37:19 +0100

    Refs #34447 - Test

--- test/unit/remote_task_test.rb
+++ test/unit/remote_task_test.rb
@@ -28,6 +28,32 @@ module ForemanTasks
           _(remote_task.remote_task_id).must_equal((remote_task.id + 5).to_s)
         end
       end
+
+      it 'honors the batches with multiple proxies' do
+        remote_task = remote_tasks.last
+        remote_task.proxy_url = 'something else'
+
+        results = remote_tasks.reduce({}) do |acc, cur|
+          acc.merge(cur.execution_plan_id.to_s => { 'task_id' => cur.id + 5, 'result' => 'success' })
+        end
+        other_results = { remote_task.execution_plan_id => results.delete(remote_task.execution_plan_id) }
+
+        fake_proxy = mock
+        fake_proxy.expects(:launch_tasks).returns(results)
+
+        another_fake_proxy = mock
+        another_fake_proxy.expects(:launch_tasks).returns(other_results)
+
+        remote_tasks.first.expects(:proxy).returns(fake_proxy)
+        remote_tasks.last.expects(:proxy).returns(another_fake_proxy)
+
+        RemoteTask.batch_trigger('a_operation', remote_tasks)
+        remote_tasks.each do |remote_task|
+          remote_task.reload
+          _(remote_task.state).must_equal 'triggered'
+          _(remote_task.remote_task_id).must_equal((remote_task.id + 5).to_s)
+        end
+      end
     end
   end
 end
