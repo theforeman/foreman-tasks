theforeman/foreman-tasks

commit be4d7851cb76e337f06bfb658ed325abfd26e57e

Fixes #32720 - Add graphql types

lib/foreman_tasks/engine.rb
@@ -33,7 +33,7 @@ module ForemanTasks
 
     initializer 'foreman_tasks.register_plugin', :before => :finisher_hook do |_app|
       Foreman::Plugin.register :"foreman-tasks" do
-        requires_foreman '>= 2.4.0'
+        requires_foreman '>= 2.6.0'
         divider :top_menu, :parent => :monitor_menu, :last => true, :caption => N_('Foreman Tasks')
         menu :top_menu, :tasks,
              :url_hash => { :controller => 'foreman_tasks/tasks', :action => :index },
@@ -64,6 +64,11 @@ module ForemanTasks
         end
 
         add_all_permissions_to_default_roles
+
+        register_graphql_query_field :task, '::Types::Task', :record_field
+        register_graphql_query_field :tasks, '::Types::Task', :collection_field
+        register_graphql_query_field :recurring_logic, '::Types::RecurringLogic', :record_field
+        register_graphql_query_field :recurring_logics, '::Types::RecurringLogic', :collection_field
 
         logger :dynflow, :enabled => true
         logger :action, :enabled => true

app/graphql/types/recurring_logic.rb
@@ -1,0 +1,18 @@
+module Types
+  class RecurringLogic < Types::BaseObject
+    description 'A Recurring Logic'
+    model_class ::ForemanTasks::RecurringLogic
+
+    global_id_field :id
+    field :cron_line, String
+    field :end_time, GraphQL::Types::ISO8601DateTime
+    field :max_iteration, Integer
+    field :iteration, Integer
+    field :state, String
+    belongs_to :triggering, Types::Triggering
+
+    def self.graphql_definition
+      super.tap { |type| type.instance_variable_set(:@name, 'ForemanTasks::RecurringLogic') }
+    end
+  end
+end

app/graphql/types/task.rb
@@ -1,0 +1,25 @@
+module Types
+  class Task < Types::BaseObject
+    description 'A Task'
+    model_class ::ForemanTasks::Task
+
+    global_id_field :id
+    field :type, String
+    field :label, String
+    field :started_at, GraphQL::Types::ISO8601DateTime
+    field :ended_at, GraphQL::Types::ISO8601DateTime
+    field :state, String
+    field :result, String
+    field :external_id, String
+    field :parent_task_id, String
+    field :start_at, GraphQL::Types::ISO8601DateTime
+    field :start_before, GraphQL::Types::ISO8601DateTime
+    field :action, String
+    field :user_id, Integer
+    field :state_updated_at, GraphQL::Types::ISO8601DateTime
+
+    def self.graphql_definition
+      super.tap { |type| type.instance_variable_set(:@name, 'ForemanTasks::Task') }
+    end
+  end
+end

app/graphql/types/triggering.rb
@@ -1,0 +1,16 @@
+module Types
+  class Triggering < Types::BaseObject
+    description 'A Task Triggering'
+    model_class ::ForemanTasks::Triggering
+
+    global_id_field :id
+    field :mode, String
+    field :start_at, GraphQL::Types::ISO8601DateTime
+    field :start_before, GraphQL::Types::ISO8601DateTime
+    field :recurring_logic, Types::RecurringLogic
+
+    def self.graphql_definition
+      super.tap { |type| type.instance_variable_set(:@name, 'ForemanTasks::Triggering') }
+    end
+  end
+end

app/models/foreman_tasks/recurring_logic.rb
@@ -4,6 +4,8 @@ module ForemanTasks
   class RecurringLogic < ApplicationRecord
     include Authorizable
 
+    graphql_type '::Types::RecurringLogic'
+
     belongs_to :task_group
     belongs_to :triggering
 

app/models/foreman_tasks/task.rb
@@ -5,6 +5,8 @@ module ForemanTasks
     include Authorizable
     extend Search
 
+    graphql_type '::Types::Task'
+
     def check_permissions_after_save
       # there's no create_tasks permission, tasks are created as a result of internal actions, in such case we
       # don't do authorization, that should have been performed on wrapping action level

app/models/foreman_tasks/triggering.rb
@@ -5,6 +5,8 @@ module ForemanTasks
               :end_time].freeze
     attr_accessor(*PARAMS)
 
+    graphql_type '::Types::Triggering'
+
     before_save do
       if future?
         parse_start_at!

test/graphql/queries/recurring_logic_test.rb
@@ -1,0 +1,28 @@
+require 'foreman_tasks_test_helper'
+
+module Queries
+  class RecurringLogicTest < GraphQLQueryTestCase
+    let(:query) do
+      <<-GRAPHQL
+      query($id: String!) {
+        recurringLogic(id: $id) {
+          id
+          cronLine
+        }
+      }
+      GRAPHQL
+    end
+
+    let(:cron_line) { '5 4 3 2 1' }
+    let(:recurring_logic) { FactoryBot.create(:recurring_logic, :cron_line => cron_line) }
+    let(:global_id) { Foreman::GlobalId.for(recurring_logic) }
+    let(:variables) { { id: global_id } }
+    let(:data) { result['data']['recurringLogic'] }
+
+    test "should fetch recurring logic" do
+      assert_empty result['errors']
+      assert_equal global_id, data['id']
+      assert_equal cron_line, data['cronLine']
+    end
+  end
+end

commit 09c0f8af1019c5dcd262d1cb1739a45b2013643b

Drop compatibility with Foreman < 2.5
The engine already requires Foreman >= 2.5 for a long time so these code
paths are dead.

Fixes: be4d7851cb76e337f06bfb658ed325abfd26e57e

app/lib/actions/foreman/host/import_facts.rb
@@ -8,12 +8,7 @@ module Actions
 
         def plan(_host_type, host_name, facts, certname, proxy_id)
           facts['domain'].try(:downcase!)
-          host = if SETTINGS[:version].short > '1.16'
-                   ::Host::Managed.import_host(host_name, certname)
-                 else
-                   # backwards compatibility
-                   ::Host::Managed.import_host(host_name, facts['_type'], certname, proxy_id)
-                 end
+          host = ::Host::Managed.import_host(host_name, certname)
           host.save(:validate => false) if host.new_record?
           action_subject(host, :facts => facts.to_unsafe_h, :proxy_id => proxy_id)
           if host.build?

app/views/foreman_tasks/recurring_logics/index.html.erb
@@ -1,5 +1,5 @@
 <% title _("Recurring logics") %>
-<% title_actions SETTINGS[:version].short <= '1.13' ? help_path : help_button %>
+<% title_actions help_button %>
 
 <% if @errors %>
 <%= alert(:class => 'alert-info', :id => 'multiple-alert', :close => false, :header => '', :text => @errors) %>

lib/foreman_tasks/engine.rb
@@ -166,7 +166,7 @@ module ForemanTasks
         world.middleware.use Actions::Middleware::KeepCurrentUser, :before => ::Dynflow::Middleware::Common::Transaction
         world.middleware.use Actions::Middleware::KeepCurrentTimezone
         world.middleware.use Actions::Middleware::KeepCurrentRequestID
-        world.middleware.use ::Actions::Middleware::LoadSettingValues if Gem::Version.new(::SETTINGS[:version]) >= Gem::Version.new('2.5')
+        world.middleware.use ::Actions::Middleware::LoadSettingValues
       end
     end
 