7e5495fee18edf9b55fba4b9879f44ed607c3eb2
Leos Stejskal <lstejska@redhat.com>
2023-01-26 14:00:14 +0100

    Fixes #36002 - Migration error 'column settings.category does not exist'

--- db/migrate/20211123170430_tasks_settings_to_dsl_category.rb
+++ db/migrate/20211123170430_tasks_settings_to_dsl_category.rb
@@ -1,5 +1,5 @@
 class TasksSettingsToDslCategory < ActiveRecord::Migration[6.0]
   def up
-    Setting.where(category: 'Setting::ForemanTasks').update_all(category: 'Setting')
+    Setting.where(category: 'Setting::ForemanTasks').update_all(category: 'Setting') if column_exists?(:settings, :category)
   end
 end
