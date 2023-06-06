478ea2979bcd4876a2429538b44df523dc9fba40
MariaAga <mariaaga@redhat.com>
2023-06-06 15:56:59 +0200

    Fixes #36476 - Remove storybook

--- webpack/ForemanTasks/Components/TaskDetails/TaskDetails.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import TaskDetails from './TaskDetails';
-
-export default {
-  title: 'TaskDetails',
-  component: TaskDetails,
-};
-
-export const Basic = () => <TaskDetails />;
