e910ea716cdfcf44bddaaa6b37f122ef3ca2e23e
kmalyjur <kmalyjur@gmail.com>
2023-09-27 08:08:50 +0000

    Fixes #36781 - Hide breadcrumb title from Tasks page

--- webpack/ForemanTasks/Components/TasksTable/TasksIndexPage.js
+++ webpack/ForemanTasks/Components/TasksTable/TasksIndexPage.js
@@ -1,10 +1,4 @@
 import React from 'react';
-import { translate as __ } from 'foremanReact/common/I18n';
 import TasksTablePage from './';
 
-export const TasksIndexPage = props => {
-  const getBreadcrumbs = () => ({
-    breadcrumbItems: [{ caption: __('Tasks'), url: `/foreman_tasks/tasks` }],
-  });
-  return <TasksTablePage getBreadcrumbs={getBreadcrumbs} {...props} />;
-};
+export const TasksIndexPage = props => <TasksTablePage {...props} />;
