121009ec96b5ae33639c859c2239a5059f3061ff
Adam Ruzicka <aruzicka@redhat.com>
2025-07-30 15:00:07 +0200

    Fixes #38626 - Hide Sinatra error pages in production

--- lib/foreman_tasks/dynflow.rb
+++ lib/foreman_tasks/dynflow.rb
@@ -18,6 +18,11 @@ module ForemanTasks
           { _('Back to tasks') => "/#{ForemanTasks::TasksController.controller_path}" }
         end
         set(:world) { ::Rails.application.dynflow.world }
+        # Do not show Sinatra's pretty error pages in production
+        set(:show_exceptions) { ::Rails.env.development? }
+        # Let the errors propagate out from Sinatra to Rails.
+        # Without this, we'd only get a mostly blank 500 ISE page
+        set(:raise_errors) { !::Rails.env.development? }
       end
     end
   end
