988ba157e7345108738e4e2fa73113fe247eab6a
MariaAga <mariaaga@redhat.com>
2025-10-15 12:26:53 +0100

    Fixes #38833 - Replace foremanModal usage with PF5 modals

--- webpack/ForemanTasks/Components/TasksTable/Components/ConfirmModal/index.js
+++ webpack/ForemanTasks/Components/TasksTable/Components/ConfirmModal/index.js
@@ -1,29 +1,6 @@
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { ConfirmModal } from './ConfirmModal';
-import reducer from './ConfirmModalReducer';
-import tasksActions from './ConfirmModalActions';
-import {
-  selectActionText,
-  selectActionState,
-  selectActionType,
-  selectClicked,
-  selectSelectedRowsLen,
-} from './ConfirmModalSelectors';
-import { selectAllRowsSelected } from '../../TasksTableSelectors';
-
-const mapStateToProps = state => ({
-  actionText: selectActionText(state),
-  actionType: selectActionType(state),
-  actionState: selectActionState(state),
-  allRowsSelected: selectAllRowsSelected(state),
-  clicked: selectClicked(state),
-  selectedRowsLen: selectSelectedRowsLen(state),
-});
-
-const mapDispatchToProps = dispatch =>
-  bindActionCreators(tasksActions, dispatch);
-
-export const reducers = { confirmModal: reducer };
-
-export default connect(mapStateToProps, mapDispatchToProps)(ConfirmModal);
+export { default as CancelModal } from './CancelModal';
+export { default as ResumeModal } from './ResumeModal';
+export { default as CancelSelectedModal } from './CancelSelectedModal';
+export { default as ResumeSelectedModal } from './ResumeSelectedModal';
+export { default as ForceUnlockModal } from './ForceUnlockModal';
+export { default as ForceUnlockSelectedModal } from './ForceUnlockSelectedModal';
