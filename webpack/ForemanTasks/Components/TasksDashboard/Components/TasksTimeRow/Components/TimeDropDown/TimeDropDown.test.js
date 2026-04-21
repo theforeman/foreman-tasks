ec0a73b14d91ec4b82fb0cc562ddc3361018d5fe
Andrei Lakatos <andreilakatos.dev@gmail.com>
2026-04-21 02:52:20 -0400

    Refs #39244 - Added RTL test for TimeDropDown component

--- webpack/ForemanTasks/Components/TasksDashboard/Components/TasksTimeRow/Components/TimeDropDown/TimeDropDown.test.js
+++ webpack/ForemanTasks/Components/TasksDashboard/Components/TasksTimeRow/Components/TimeDropDown/TimeDropDown.test.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import { render, screen, fireEvent, waitFor, within } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import { testComponentSnapshotsWithFixtures } from '@theforeman/test';
 
 import { TASKS_DASHBOARD_AVAILABLE_TIMES } from '../../../../TasksDashboardConstants';
@@ -17,3 +20,52 @@ const fixtures = {
 
 describe('TimeDropDown', () =>
   testComponentSnapshotsWithFixtures(TimeDropDown, fixtures));
+
+describe('TimeDropDown interactions', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('calls onChange when a different time option is selected', async () => {
+    const onChange = jest.fn();
+
+    render(
+      <TimeDropDown
+        id="time-dropdown"
+        selectedTime={TASKS_DASHBOARD_AVAILABLE_TIMES.H24}
+        onChange={onChange}
+      />
+    );
+
+    fireEvent.click(screen.getByRole('button', { name: /24h/i }));
+
+    const menu = await screen.findByRole('menu');
+    fireEvent.click(within(menu).getByRole('menuitem', { name: /^12h$/i }));
+
+    await waitFor(() => {
+      expect(onChange).toHaveBeenCalledTimes(1);
+      expect(onChange).toHaveBeenCalledWith(
+        TASKS_DASHBOARD_AVAILABLE_TIMES.H12
+      );
+    });
+  });
+
+  it('does not call onChange when the already-selected option is clicked', async () => {
+    const onChange = jest.fn();
+
+    render(
+      <TimeDropDown
+        id="time-dropdown"
+        selectedTime={TASKS_DASHBOARD_AVAILABLE_TIMES.WEEK}
+        onChange={onChange}
+      />
+    );
+
+    fireEvent.click(screen.getByRole('button', { name: /^week$/i }));
+
+    const menu = await screen.findByRole('menu');
+    fireEvent.click(within(menu).getByRole('menuitem', { name: /^week$/i }));
+
+    expect(onChange).not.toHaveBeenCalled();
+  });
+});
