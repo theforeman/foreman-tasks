8bc6ea43d824dc9df9c75f33b65433373c09ea24
Andrei Lakatos <andreilakatos.dev@gmail.com>
2026-05-08 02:07:06 -0400

    Fixes #39294 - PF5 Locks and RTL tests

--- webpack/ForemanTasks/Components/TaskDetails/Components/Locks.js
+++ webpack/ForemanTasks/Components/TaskDetails/Components/Locks.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Alert, Card, Row, Col } from 'patternfly-react';
+import {
+  Alert,
+  AlertVariant,
+  Gallery,
+  GalleryItem,
+  Card,
+  CardTitle,
+  CardBody,
+} from '@patternfly/react-core';
 import { translate as __ } from 'foremanReact/common/I18n';
 
 const ConditionalLink = ({ children, link }) =>
@@ -17,35 +25,36 @@ ConditionalLink.defaultProps = {
 
 const Locks = ({ locks }) => (
   <div>
-    <Alert type="info">
+    <Alert
+      variant={AlertVariant.info}
+      ouiaId="task-locks-info"
+      isInline
+      className="pf-u-mb-lg"
+    >
       {__(
         'You can find resource locks on this page. Exclusive lock marked with locked icon means that no other task can use locked resource while this task is running. Non-exclusive lock marked with unlocked icon means other tasks can access the resource freely, it is only used to indicate the relation of this task with the resource'
       )}
     </Alert>
-    <Card.Grid>
-      <Row>
-        {locks.map((lock, key) => (
-          <Col xs={6} sm={4} md={4} key={key}>
-            <ConditionalLink link={lock.link}>
-              <Card className="card-pf-aggregate-status" accented>
-                <Card.Title>
-                  <span
-                    className={`fa ${
-                      lock.exclusive ? 'fa-lock' : 'fa-unlock-alt'
-                    }`}
-                  />
-                  {lock.resource_type}
-                </Card.Title>
-                <Card.Body>
-                  {`id:${lock.resource_id}`}
-                  <br />
-                </Card.Body>
-              </Card>
-            </ConditionalLink>
-          </Col>
-        ))}
-      </Row>
-    </Card.Grid>
+    <Gallery hasGutter>
+      {locks.map((lock, key) => (
+        <GalleryItem key={key}>
+          <ConditionalLink link={lock.link}>
+            <Card isCompact ouiaId={`task-lock-${key}`}>
+              <CardTitle>
+                <span
+                  className={`pf-u-mr-sm fa ${
+                    lock.exclusive ? 'fa-lock' : 'fa-unlock-alt'
+                  }`}
+                  aria-hidden
+                />
+                {lock.resource_type}
+              </CardTitle>
+              <CardBody>{`id:${lock.resource_id}`}</CardBody>
+            </Card>
+          </ConditionalLink>
+        </GalleryItem>
+      ))}
+    </Gallery>
   </div>
 );
 
