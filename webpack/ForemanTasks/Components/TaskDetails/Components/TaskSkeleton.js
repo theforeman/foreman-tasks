da34a11c93dd8acb4e5c5de2b7e7801eeabcb92c
Andrei Lakatos <andreilakatos.dev@gmail.com>
2026-05-08 02:07:06 -0400

    Fixes #39294 - PF5 TaskDetails shell, skeleton, actions tests, Task/Raw/Dependencies RTL tests

--- webpack/ForemanTasks/Components/TaskDetails/Components/TaskSkeleton.js
+++ webpack/ForemanTasks/Components/TaskDetails/Components/TaskSkeleton.js
@@ -1,48 +1,50 @@
 import React from 'react';
 import Skeleton from 'react-loading-skeleton';
-import { Grid, Row, Col } from 'patternfly-react';
+import { Grid, GridItem } from '@patternfly/react-core';
 
 export const TaskSkeleton = () => {
-  const details = [1, 2, 3, 4, 5, 6];
+  const details = [1, 2, 3, 4];
   return (
-    <Grid>
-      <br />
-      <Row>
-        <Col>
-          <Skeleton />
-        </Col>
-      </Row>
-      {details.map((items, key) => (
-        <Row key={key}>
-          <Col md={2} sm={6}>
+    <Grid hasGutter>
+      <GridItem span={12}>
+        <br />
+      </GridItem>
+      <GridItem span={12}>
+        <Skeleton />
+      </GridItem>
+      {details.map(key => (
+        <React.Fragment key={key}>
+          <GridItem md={2} sm={6}>
             <Skeleton />
-          </Col>
-          <Col md={5} sm={6}>
+          </GridItem>
+          <GridItem md={5} sm={6}>
             <Skeleton />
-          </Col>
-          <Col md={2} sm={6}>
+          </GridItem>
+          <GridItem md={2} sm={6}>
             <Skeleton />
-          </Col>
-          <Col md={3} sm={6}>
+          </GridItem>
+          <GridItem md={3} sm={6}>
             <Skeleton />
-          </Col>
-        </Row>
+          </GridItem>
+        </React.Fragment>
       ))}
-      <br />
-      <Row>
-        <Col xs={6}>
-          <div className="progress-description">
-            <Skeleton />
-          </div>
-        </Col>
-        <Col xs={3} xsOffset={3} className="progress-label-top-right">
+      <GridItem span={12}>
+        <br />
+      </GridItem>
+      <GridItem span={6}>
+        <div className="progress-description">
           <Skeleton />
-        </Col>
-        <Col xs={12}>
-          <Skeleton />
-        </Col>
-      </Row>
-      <br />
+        </div>
+      </GridItem>
+      <GridItem span={6} className="progress-label-top-right">
+        <Skeleton />
+      </GridItem>
+      <GridItem span={12}>
+        <Skeleton />
+      </GridItem>
+      <GridItem span={12}>
+        <br />
+      </GridItem>
     </Grid>
   );
 };
