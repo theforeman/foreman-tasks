ea89fefdbf9ad6851317736b56515df0442e4ab7
Andrei Lakatos <andreilakatos.dev@gmail.com>
2026-05-11 12:22:10 -0400

    Refs #39294 - used danger variant, removed title case

--- webpack/ForemanTasks/Components/TaskDetails/Components/RunningSteps.js
+++ webpack/ForemanTasks/Components/TaskDetails/Components/RunningSteps.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Alert, AlertVariant, Button } from '@patternfly/react-core';
-import { translate as __, sprintf } from 'foremanReact/common/I18n';
+import { translate as __ } from 'foremanReact/common/I18n';
 
 const RunningSteps = ({
   runningSteps,
@@ -13,57 +13,51 @@ const RunningSteps = ({
   if (!runningSteps.length) return <span>{__('No running steps')}</span>;
   return (
     <div>
-      {runningSteps.map((step, i) => {
-        const title =
-          typeof step.title === 'string' && step.title.trim() !== ''
-            ? step.title.trim()
-            : sprintf(__('Running step %s'), i + 1);
-        return (
-          <Alert
-            variant={AlertVariant.warning}
-            isInline
-            key={step.id || i}
-            ouiaId={`running-step-${i}`}
-            title={title}
-          >
-            {step.cancellable && (
-              <p>
-                <Button
-                  variant="secondary"
-                  size="sm"
-                  ouiaId={`running-step-cancel-button-${i}`}
-                  onClick={() => {
-                    if (!taskReload) {
-                      taskReloadStart(id);
-                    }
-                    cancelStep(id, step.id);
-                  }}
-                >
-                  {__('Cancel')}
-                </Button>
-              </p>
-            )}
+      {runningSteps.map((step, i) => (
+        <Alert
+          variant={AlertVariant.warning}
+          isInline
+          key={step.id || i}
+          ouiaId={`running-step-${i}`}
+          title={__(`Running step ${i + 1}`)}
+        >
+          {step.cancellable && (
+            <p>
+              <Button
+                variant="danger"
+                size="sm"
+                ouiaId={`running-step-cancel-button-${i}`}
+                onClick={() => {
+                  if (!taskReload) {
+                    taskReloadStart(id);
+                  }
+                  cancelStep(id, step.id);
+                }}
+              >
+                {__('Cancel')}
+              </Button>
+            </p>
+          )}
 
-            <p>
-              <span>{__('Action')}:</span>
-              <span />
-            </p>
-            <pre>{step.action_class}</pre>
-            <p>
-              <span>{__('State')}:</span>
-              <span>{step.state}</span>
-            </p>
-            <span>{__('Input')}:</span>
-            <span>
-              <pre>{step.input}</pre>
-            </span>
-            <span>{__('Output')}:</span>
-            <span>
-              <pre>{step.output}</pre>
-            </span>
-          </Alert>
-        );
-      })}
+          <p>
+            <span>{__('Action')}:</span>
+            <span />
+          </p>
+          <pre>{step.action_class}</pre>
+          <p>
+            <span>{__('State')}:</span>
+            <span>{step.state}</span>
+          </p>
+          <span>{__('Input')}:</span>
+          <span>
+            <pre>{step.input}</pre>
+          </span>
+          <span>{__('Output')}:</span>
+          <span>
+            <pre>{step.output}</pre>
+          </span>
+        </Alert>
+      ))}
     </div>
   );
 };
