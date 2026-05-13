10121d6b553aadb329fee8f1f74373e65e326e21
andreilakatos <andreilakatos.dev@gmail.com>
2026-05-13 17:38:32 +0300

    Fixes #39294 - PF5 RunningSteps and RTL tests (#809)
    * Fixes #39294 - PF5 RunningSteps and RTL tests
    
    * Refs #39294 - used danger variant, removed title case
    
    * Refs #39294 - added sprintf and tests
    
    * Refs #39294 - removed unnecessary mock

--- webpack/ForemanTasks/Components/TaskDetails/Components/RunningSteps.js
+++ webpack/ForemanTasks/Components/TaskDetails/Components/RunningSteps.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Alert, Button } from 'patternfly-react';
-import { translate as __ } from 'foremanReact/common/I18n';
+import { Alert, AlertVariant, Button } from '@patternfly/react-core';
+import { translate as __, sprintf } from 'foremanReact/common/I18n';
 
 const RunningSteps = ({
   runningSteps,
@@ -14,11 +14,19 @@ const RunningSteps = ({
   return (
     <div>
       {runningSteps.map((step, i) => (
-        <Alert type="warning" key={i}>
+        <Alert
+          variant={AlertVariant.warning}
+          isInline
+          key={step.id || i}
+          ouiaId={`running-step-${i}`}
+          title={sprintf(__('Running step %s'), i + 1)}
+        >
           {step.cancellable && (
             <p>
               <Button
-                bsSize="small"
+                variant="danger"
+                size="sm"
+                ouiaId={`running-step-cancel-button-${i}`}
                 onClick={() => {
                   if (!taskReload) {
                     taskReloadStart(id);
