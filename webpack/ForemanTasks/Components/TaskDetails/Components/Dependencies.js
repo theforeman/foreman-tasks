567a85826c07e49c0909d6e38e33e84711c98a3c
Andrei Lakatos <andreilakatos.dev@gmail.com>
2026-05-15 06:32:04 -0400

    Refs #39294 - used pf5 components instead of pf5 classes

--- webpack/ForemanTasks/Components/TaskDetails/Components/Dependencies.js
+++ webpack/ForemanTasks/Components/TaskDetails/Components/Dependencies.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { capitalize } from 'lodash';
-import { Stack, Text, TextVariants, Title } from '@patternfly/react-core';
+import {
+  Grid,
+  GridItem,
+  Stack,
+  Text,
+  TextVariants,
+  Title,
+} from '@patternfly/react-core';
 import { Table, Thead, Tbody, Tr, Th, Td } from '@patternfly/react-table';
 import { translate as __ } from 'foremanReact/common/I18n';
 
@@ -10,45 +17,48 @@ const DependencyTable = ({ title, tasks, ouiaSectionId }) => (
     <Title headingLevel="h3" size="lg" ouiaId={`${ouiaSectionId}-title`}>
       {title}
     </Title>
-    {tasks.length === 0 ? (
-      <p className="text-muted">{__('None')}</p>
-    ) : (
-      <Table
-        aria-label={title}
-        variant="compact"
-        ouiaId={`${ouiaSectionId}-table`}
-        className="pf-v5-u-w-100-on-md pf-v5-u-w-66-on-xl"
-      >
-        <Thead>
-          <Tr ouiaId={`${ouiaSectionId}-table-header`}>
-            <Th>{__('Name')}</Th>
-            <Th>{__('State')}</Th>
-            <Th>{__('Result')}</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {tasks.map(task => (
-            <Tr key={task.id} ouiaId={`${ouiaSectionId}-table-row-${task.id}`}>
-              <Td dataLabel={__('Name')}>
-                <Text
-                  component={TextVariants.a}
-                  href={`/foreman_tasks/tasks/${task.id}`}
-                  ouiaId={`${ouiaSectionId}-task-link-${task.id}`}
-                >
-                  {task.humanized || task.action}
-                </Text>
-              </Td>
-              <Td dataLabel={__('State')}>
-                {capitalize(String(task.state || ''))}
-              </Td>
-              <Td dataLabel={__('Result')}>
-                {capitalize(String(task.result || ''))}
-              </Td>
-            </Tr>
-          ))}
-        </Tbody>
-      </Table>
-    )}
+    <Grid hasGutter>
+      <GridItem span={12} xl={8}>
+        {tasks.length === 0 ? (
+          <Text component={TextVariants.small}>{__('None')}</Text>
+        ) : (
+          <Table
+            aria-label={title}
+            variant="compact"
+            ouiaId={`${ouiaSectionId}-table`}
+          >
+            <Thead>
+              <Tr ouiaId={`${ouiaSectionId}-table-header`}>
+                <Th>{__('Name')}</Th>
+                <Th>{__('State')}</Th>
+                <Th>{__('Result')}</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {tasks.map(task => (
+                <Tr key={task.id} ouiaId={`${ouiaSectionId}-table-row-${task.id}`}>
+                  <Td dataLabel={__('Name')}>
+                    <Text
+                      component={TextVariants.a}
+                      href={`/foreman_tasks/tasks/${task.id}`}
+                      ouiaId={`${ouiaSectionId}-task-link-${task.id}`}
+                    >
+                      {task.humanized || task.action}
+                    </Text>
+                  </Td>
+                  <Td dataLabel={__('State')}>
+                    {capitalize(String(task.state || ''))}
+                  </Td>
+                  <Td dataLabel={__('Result')}>
+                    {capitalize(String(task.result || ''))}
+                  </Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        )}
+      </GridItem>
+    </Grid>
   </Stack>
 );
 
